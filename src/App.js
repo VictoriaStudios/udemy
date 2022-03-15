e92e4c2885c23eecbc3bf8cb1c33bcaf24f00313
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2022-03-15 14:13:05 -0500

    Tried out many features, including custom buttons, modals, React portals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ErrorModal } from './components/ErrorModal';
+import Button from './components/Button';
 
 function App() {
+  const [errorOpen, setErrorOpen] = useState(false)
+  const handleOpenErrorModal = () => setErrorOpen (true)
+  const handleCloseErrorModal = () => setErrorOpen (false)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
+      <Button type="submit" onClick={handleOpenErrorModal}>
+        Hi
+      </Button>
+      {errorOpen? (<ErrorModal onClose={handleCloseErrorModal} />): ("")}
     </div>
   );
 }
